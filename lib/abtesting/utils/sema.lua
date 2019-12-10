58df5e40ceb42dad9f2c3cde63568de3bf5624d9
geikiy <geikiy@qq.com>
2019-12-10 13:14:21 +0800

    delete lua-resty-core and resty dependencies library

--- lib/abtesting/utils/sema.lua
+++ lib/abtesting/utils/sema.lua
@@ -1,9 +1,10 @@
 local modulename = "abtestingSema"
 local _M = {}
 
-local semaphore = require("lua-resty-core.lib.ngx.semaphore")
+local semaphore = require("ngx.semaphore")
 
 _M.sema = semaphore.new(1)
 _M.upsSema = semaphore.new(1)
+_M.modulename = modulename
 
 return _M
