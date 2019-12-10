c5ff59f4b199cdc550dda73f0cd6b1a099227a3e
geikiy <geikiy@qq.com>
2019-12-10 14:18:17 +0800

    modfiy start/stop openresty script

--- utils/stop-all-nginx.sh
+++ utils/stop-all-nginx.sh
@@ -1,8 +1,17 @@
 rm *temp -rf
 
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/nginx.conf  -s stop
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/stable.conf -s stop
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta1.conf  -s stop
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta2.conf  -s stop
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta3.conf  -s stop
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta4.conf  -s stop
+#NGINX="/data/openresty1915/nginx/sbin/nginx"
+NGINX="/usr/local/openresty/nginx/sbin/nginx"
+$NGINX -p `pwd` -c conf/nginx.conf -s stop
+$NGINX -p `pwd` -c conf/stable.conf -s stop
+$NGINX -p `pwd` -c conf/beta1.conf -s stop
+$NGINX -p `pwd` -c conf/beta2.conf -s stop
+$NGINX -p `pwd` -c conf/beta3.conf -s stop
+$NGINX -p `pwd` -c conf/beta4.conf -s stop
+
+#/usr/local/nginx/sbin/nginx -p `pwd` -c conf/nginx.conf  -s stop
+#/usr/local/nginx/sbin/nginx -p `pwd` -c conf/stable.conf -s stop
+#/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta1.conf  -s stop
+#/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta2.conf  -s stop
+#/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta3.conf  -s stop
+#/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta4.conf  -s stop
